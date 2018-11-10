9b653b4e0a423ed5843d8d5a827a7af05bebd95f
hatodev <haiphamnguyenhoang@gmail.com>
2018-11-10 13:17:25 +0700

    simple http server

--- server/server.js
+++ server/server.js
@@ -4,3 +4,5 @@ http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello World!\nCE boys are handsome\n');
 }).listen(8000);
+
+console.log("Server is running");
