768593e3a573e7b1bb3a95cc23fd529dd23b33d3
hatodev <haiphamnguyenhoang@gmail.com>
2018-11-10 14:33:04 +0700

    server

--- server/server.js
+++ server/server.js
@@ -1,8 +1,36 @@
-var http = require('http');
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var ip = require('ip');
+var io = require('socket.io');
+var fs = require('fs');
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!\nCE boys are handsome\n');
-}).listen(8000);
+app.get('/', function (req, res){
+   fs.readFile('./' + req.url, function(err, data) {
+        if (!err) {
+            var dotoffset = req.url.lastIndexOf('.');
+            var mimetype = dotoffset == -1
+                            ? 'text/plain'
+                            : {
+                                '.html' : 'text/html',
+                                '.ico' : 'image/x-icon',
+                                '.jpg' : 'image/jpeg',
+                                '.png' : 'image/png',
+                                '.gif' : 'image/gif',
+                                '.css' : 'text/css',
+                                '.js' : 'text/javascript'
+                                }[ req.url.substr(dotoffset) ];
+            res.setHeader('Content-type' , mimetype);
+            res.end(data);
+            console.log( req.url, mimetype );
+        } else {
+            console.log ('file not found: ' + req.url);
+            res.writeHead(404, "Not Found");
+            res.end();
+        }
+    });
+});
 
-console.log("Server is running. Try to press ^A then ^D.");
+http.listen(80, function(){
+   console.log('Server starts at ' + ip.address() + ':80');
+});
